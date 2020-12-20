43d60123d1b03845910891d193ce34dfba5baac8
Wesley <wes.hamster@gmail.com>
2020-12-20 01:24:35 -0500

    Added config loader. Changed motion detection

--- VideoDevice.py
+++ VideoDevice.py
@@ -5,118 +5,12 @@ import numpy as np
 import queue
 import threading
 import collections
+import imutils
 
 historyLen = 30
 videoCodec = cv2.VideoWriter_fourcc("D", "I", "V", "X")
 videoCaptureTime = 10
 scriptDir = os.path.dirname(sys.argv[0])
-
-
-class VideoDevice:
-    def __init__(self, name, url, fps, sensitivity):
-        self.name = name
-        self.prevFrame = None
-        self.frame = None
-        self.url = url
-        self.cap = VideoCapture(self.url)
-        self.fps = fps
-        self.history = collections.deque(maxlen=historyLen)
-        self.threshold = 0
-        self.sensitivity = sensitivity
-
-        print(name, "initialized with", self.fps, " fps")
-
-    def ShowFrames(self, images):
-        outputImage = np.vstack(images)
-        cv2.imshow(self.name, outputImage)
-        cv2.waitKey(250)
-        #cv2.destroyAllWindows()
-
-    def ShowFrame(self, image):
-        self.ShowFrames([image])
-
-    def GetFrame(self):
-        print("Getting frame...")
-
-        frame = self.cap.read()
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-        #frame = cv2.fastNlMeansDenoising(frame, None, 10, 10, 7, 21)
-        #frame = cv2.blur(frame, (15, 15))
-
-        self.prevFrame = self.frame
-        self.frame = frame
-
-        #self.ShowFrames([blurImage, frame])
-        #self.ShowFrame(frame)
-
-        #kernel = np.ones((5, 5), np.uint8)
-        #blurImage = cv2.dilate(frame, kernel, iterations = 1)
-        
-        #self.ShowFrame(blurImage)
-        
-        #cap.release()
-
-        self.CompareFrames()
-        
-    def CalcDiff(self, frame1, frame2):
-        difference = cv2.subtract(frame1, frame2)
-        self.ShowFrame(difference)
-        diff = difference.sum()
-        sum = np.sum(diff)
-        return sum
-
-    def UpdateThreshold(self, n):
-        self.history.append(n)
-        self.threshold = int(sum(self.history) / len(self.history))
-
-    def IsOverThreshold(self, n):
-        #highest = max(self.history)
-        return (n > self.threshold * self.sensitivity)
-
-    def CompareFrames(self):
-        print("Comparing")
-        if self.frame is None or self.prevFrame is None:
-            return None
-
-        diff = self.CalcDiff(self.frame, self.prevFrame)
-        self.UpdateThreshold(diff)
-
-        print(diff)
-        print("Current threshold:", self.threshold)
-
-        if len(self.history) < self.history.maxlen:
-            print("Not enough yet")
-            return
-
-        if self.IsOverThreshold(diff):
-            print("Saving video...")
-            fileName = self.SaveVideo()
-            print("Saved to", fileName)
-            time.sleep(1)
-
-        
-
-    def SaveVideo(self):
-        startTime = time.time()
-        fileName = self.name + "-" + str(time.time()) + ".avi"
-        fileName = os.path.join(scriptDir, fileName)
-
-        height, width = self.frame.shape[:2]
-        print("Size: ", width, "x", height)
-
-        videoWriter = cv2.VideoWriter(fileName, videoCodec, self.fps, (width, height))
-
-        while time.time() - startTime < videoCaptureTime:
-            videoWriter.write(self.cap.read())
-
-        videoWriter.release()
-
-        return fileName
-
-
-
-
 
 
 # bufferless VideoCapture
@@ -148,3 +42,116 @@ class VideoCapture:
 
     def get(self, what):
         return self.cap.get(what)
+
+class Box:
+    def __init__(self, x, y, w, h):
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+
+    def GetArea(self):
+        return self.w * self.h
+
+class VideoDevice:
+    def __init__(self, name, url, fps, sensitivity):
+        print("Initializing", name)
+        self.name = name
+        self.firstFrame = None
+        self.url = url
+        self.cap = VideoCapture(self.url)
+        self.fps = fps
+        self.history = collections.deque(maxlen=historyLen)
+        self.threshold = 0
+        self.sensitivity = sensitivity
+
+        print(name, "initialized with", self.fps, " fps")
+
+    def ShowFrames(self, images):
+        outputImage = np.vstack(images)
+        cv2.imshow(self.name, outputImage)
+        cv2.waitKey(250)
+        #cv2.destroyAllWindows()
+
+    def ShowFrame(self, image):
+        self.ShowFrames([image])
+
+    def GetBiggestBox(self, boxes):
+        biggest = None
+        for box in boxes:
+            if biggest == None or box.GetArea() > biggest.GetArea():
+                biggest = box
+
+        return biggest
+        
+
+    def GetBoxes(self, frame):
+        boxes = []
+
+        cnts = cv2.findContours(frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        cnts = imutils.grab_contours(cnts)
+
+        for c in cnts:
+            if cv2.contourArea(c) < 500:
+                continue
+            (x, y, w, h) = cv2.boundingRect(c)
+            boxes.append(Box(x, y, w, h))
+
+        return boxes
+
+
+    def Update(self):
+        frame = self.cap.read()
+        text = "Unoccupied"
+
+        if frame is None:
+            return
+
+        frame = imutils.resize(frame, width=500)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        gray = cv2.GaussianBlur(gray, (21, 21), 0)
+
+        if self.firstFrame is None:
+            self.firstFrame = gray
+            return
+
+        frameDelta = cv2.absdiff(self.firstFrame, gray)
+
+        thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
+        thresh = cv2.dilate(thresh, None, iterations=2)
+
+        boxes = self.GetBoxes(thresh)
+        for box in boxes:
+            cv2.rectangle(frame, (box.x, box.y), (box.x + box.w, box.y + box.h), (0, 255, 0), 2)
+
+        biggest = self.GetBiggestBox(boxes)
+        if biggest:
+            cv2.rectangle(frame, (biggest.x, biggest.y), (biggest.x + biggest.w, biggest.y + biggest.h), (0, 0, 255), 2)
+            text = "Occupied"
+
+        cv2.putText(frame, "Room Status: {}".format(text), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+
+        cv2.imshow(self.name + " Security Feed", frame)
+        cv2.imshow(self.name + " Thresh", thresh)
+        cv2.imshow(self.name + " Frame Delta", frameDelta)
+        cv2.waitKey(1)
+
+        
+
+    def SaveVideo(self):
+        startTime = time.time()
+        fileName = self.name + "-" + str(time.time()) + ".avi"
+        fileName = os.path.join(scriptDir, fileName)
+
+        height, width = self.firstFrame.shape[:2]
+        print("Size: ", width, "x", height)
+
+        videoWriter = cv2.VideoWriter(fileName, videoCodec, self.fps, (width, height))
+
+        while time.time() - startTime < videoCaptureTime:
+            videoWriter.write(self.cap.read())
+
+        videoWriter.release()
+
+        return fileName
+
