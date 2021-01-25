6b227b6a119aaf74e94ab4044d1198450abc86b2
Wes <wes.hamster@gmail.com>
2021-01-24 19:26:33 -0500

    improved sidenav, added pages

--- Webserver.py
+++ Webserver.py
@@ -4,8 +4,16 @@ import ConfigManager as config
 app = Flask(__name__, static_folder="web/static", template_folder="web/templates")
 
 @app.route('/')
-def index():
+def indexPage():
     return render_template("index.html")
+
+@app.route('/cameras')
+def camerasPage():
+    return render_template("cameras.html")
+
+@app.route('/config')
+def configPage():
+    return render_template("config.html")
 
 def Run(debug=False):
     port = config.GetValueR("webserver", "port")
